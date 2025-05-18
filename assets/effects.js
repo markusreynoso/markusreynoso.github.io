1a9ad98e4fabeb85c7ce166f4f571a43b0d57c61
markusreynoso <markus.reynoso.c@gmail.com>
2025-05-18 17:41:58 +0800

    Add others section

--- assets/effects.js
+++ assets/effects.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-    $("#research-title").hover(
-        function () {
-            $(this).text("Research")
-        },
-        function () {
-            $(this).text("Coming soon")
-        }
-    )
-
     const cardSound = new Audio("assets/audio/card-sound.mp3")
     $(".role-card").on("mouseenter", function (){
         cardSound.currentTime = 0;
