61765223795b01c475e533cbec0e4de57e1ca01f
markusreynoso <markus.reynoso.c@gmail.com>
2025-05-22 20:31:31 +0800

    Finish other projects section

--- assets/effects.js
+++ assets/effects.js
@@ -1,7 +1,50 @@
 $(document).ready(function () {
+    // Sound effect when hovering on the role cards
     const cardSound = new Audio("assets/audio/card-sound.mp3")
-    $(".role-card").on("mouseenter", function (){
+    $(".role-card").on("mouseenter", function () {
         cardSound.currentTime = 0;
         cardSound.play();
     })
+
+
+    // Displaying images on the cursor when hovering on other projects
+    $('.others-row').on('mouseenter', function () {
+        if (window.innerWidth >= 768) {
+            const imgSrc = $(this).data('image');
+            $('#cursor-image').attr('src', `assets/images/${imgSrc}`).removeClass('hidden');
+        }
+    });
+
+    $('.others-row').on('mousemove', function (e) {
+        if (window.innerWidth >= 768) {
+            $('#cursor-image').css({
+                left: (e.clientX + 30) + 'px',
+                top: (e.clientY - 50) + 'px'
+            });
+        }
+    });
+
+    $('.others-row').on('mouseleave', function () {
+        if (window.innerWidth >= 768) {
+            $('#cursor-image').addClass('hidden');
+        }
+    });
+
+
+    // Display the right arrow when hovering on other project
+    $('.others-row').on('mouseenter', function () {
+        if (window.innerWidth >= 768) {
+            $(this).find('.other-project-title').toggleClass('fc-accent');
+            $(this).find('.bi-arrow-right-short').toggleClass('hidden');
+        }
+    });
+
+    $('.others-row').on('mouseleave', function () {
+        if (window.innerWidth >= 768) {
+            $(this).find('.other-project-title').toggleClass('fc-accent');
+            $(this).find('.bi-arrow-right-short').toggleClass('hidden');
+        }
+    });
+
+
 })
--- assets/redirects.js
+++ assets/redirects.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Redirect icons in the landing div
     $("#linkedin-icon").click(function () {
         window.open("https://www.linkedin.com/in/markus-reynoso/", "_blank", "noopener");
     })
@@ -11,9 +12,15 @@ $(document).ready(function () {
         window.open("https://www.behance.net/markus_designs", "_blank", "noopener");
     })
 
-    $(".thumbnail-container, .project-title").click(function(){
+    $(".thumbnail-container, .project-title").click(function () {
         const url = $(this).data("url")
-        console.log(url)
+        window.open(url, "_blank", "noopener");
+    })
+
+
+    // Redirects in other projects
+    $('.others-row').click(function () {
+        const url = $(this).data('url');
         window.open(url, "_blank", "noopener");
     })
 })
